b26ffb0f7ac37e70c58d51ab4f044970c0682aba
AustinCreates154 <41809488+AustinCreates154@users.noreply.github.com>
2018-09-11 11:44:21 -0400

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,27 @@
+cosnt discord = require('discord.js');
+var bot = new discord.Client();
+
+require('fs').readdir("./commands/", (err, files) => {
+  console.log("Loading commands...");
+  if (err) return console.log(`Command loading failed!`);
+  files.filter(f => f.split(".").pop() === "js").forEach((f, i) => {
+    bot.commands.set(require(`./commands/${f}`).help.name, require(`./commands/${f}`));
+  });
+});
+
+bot.on("ready", () => {
+  console.log("I am logged on.")
+  bot.user.setActivity("Waiting to log orders!")
+})
+
+bot.on('message', message => {
+  let mArray = message.content.split(" ")
+  let args = mArray.slice(1)
+  let cmd = bot.commands.get(mArray[0].slice(prefix.length))
+  if (message.author.bot) return;
+  if (message.channel.type == "dm") return;
+  if (!message.content.startsWith(prefix)) return;
+  
+  if (cmd) {
+    cmd.run(bot, message, args, discord)
+})
